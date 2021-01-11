d0b68f4d943a6f4f37474941feee8806d4d6a9f0
Hwang08 <pmhwang8@gmail.com>
2021-01-11 14:46:31 -0500

    add repeat background image

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// Lesson 146
-
 // Looks for how many elements have the class of .drum
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
@@ -18,65 +16,8 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
         makeSound(buttonInnerHTML);
 
         buttonAnimation(buttonInnerHTML);
-
-        // Prints the button object
-        // console.log(this);
-
-        // Prints the innerHTML
-        // console.log(this.innerHTML);
-
-        // Whenever you click a button it changes the text color to white.
-        // this.style.color = "white";
-
-        // Note. The constructor function for the Audio function in javascript might look like
-        // function Audio (fileLocation){
-        //     this.fileLocation = fileLocation;
-        //     this.play = function(){
-        //         //Tap into the audio hardware
-        //         // Check the file at fileLocation exists
-        //         // Check the file at fileLocation is a sound file
-        //         // Play the file at fileLocation
-        //     }
-        // }
-
-
     });
 
-    // -------------------------------------------
-    // Cut and pasted the switch statement into the makeSound(key) function
-    // -------------------------------------------
-
-    // This was the first way I thought of making each button work seperatly before the switch statement. 
-
-    // document.querySelectorAll(".w")[i].addEventListener("click", function () {
-    //     console.log(this);
-    //     var tom1 = new Audio("sounds/tom-1.mp3");
-    //     tom1.play();
-    // });
-    // document.querySelectorAll(".a")[i].addEventListener("click", function () {
-    //     var tom1 = new Audio("sounds/tom-2.mp3");
-    //     tom1.play();
-    // });
-    // document.querySelectorAll(".s")[i].addEventListener("click", function () {
-    //     var tom1 = new Audio("sounds/tom-3.mp3");
-    //     tom1.play();
-    // });
-    // document.querySelectorAll(".d")[i].addEventListener("click", function () {
-    //     var tom1 = new Audio("sounds/tom-4.mp3");
-    //     tom1.play();
-    // });
-    // document.querySelectorAll(".j")[i].addEventListener("click", function () {
-    //     var tom1 = new Audio("sounds/snare.mp3");
-    //     tom1.play();
-    // });
-    // document.querySelectorAll(".k")[i].addEventListener("click", function () {
-    //     var tom1 = new Audio("sounds/crash.mp3");
-    //     tom1.play();
-    // });
-    // document.querySelectorAll(".l")[i].addEventListener("click", function () {
-    //     var tom1 = new Audio("sounds/kick-bass.mp3");
-    //     tom1.play();
-    // });
 }
 
 // Detects keyboard presses.
@@ -152,48 +93,3 @@ function buttonAnimation(currentKey) {
         activeButton.classList.remove("pressed");
     }, 100);
 }
-
-// Adds event listener to the first button.
-// document.querySelector("button")[0].addEventListener("click", function () {
-//     alert("I got clicked!");
-
-// });
-
-// // Object
-// ---------------------------------------------------
-
-// var car = {
-//     type: "sedan",
-//     company: "audi",
-//     colors: ["black", "brown"]
-// }
-
-// // Constructor function -  1st letter in the function name is capitalized
-// this(object).type = type(type that is given as an input)
-// ---------------------------------------------------
-
-// function Car(type, company, colors) {
-//     this.type = type;
-//     this.company = company;
-//     this.colors = colors;
-// }
-
-// // Initializing an object
-// // The only difference between this syntax and calling a function is the new keyword and the fact that the name of the function is capitalized
-// ---------------------------------------------------
-
-// var car = new Car("suv", "Ford", ["Silver", "Black"]);
-
-// Method - A method is a function that is associated with an object or inside an object. In this case provide the name of the function as a new parameter in the object and then put a colon and then put the anonymous function.
-// ---------------------------------------------------
-
-// var car = {
-//     type: "sedan",
-//     company: "audi",
-//     colors: ["black", "brown"],
-//     // This is a method- name of the function as a new parameter, and then the anonymous function
-//     move: function(){
-//         alert("Car Moving");
-//         move();
-//     }
-// }
